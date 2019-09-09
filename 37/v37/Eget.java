73dbb344c0e1844e6c833e046e54d398fcf32571
ParsaFIlip2002 <filip.parsa@elev.ga.ntig.se>
2019-09-09 13:13:20 +0200

    v37 må

---
+++ 37/v37/Eget.java
@@ -0,0 +1,14 @@
+package v37;
+
+public class Eget {
+	public static void main(String[] args) {
+		
+		long helTal1 = 200;
+		
+		long svar =  helTal1*helTal1;
+		System.out.println( svar);
+		
+	}
+	
+
+}
---
+++ 37/v37/Lektion1.java
@@ -0,0 +1,27 @@
+package v37;
+
+public class Lektion1 {
+
+	public static void main(String[] args) { 
+		// TODO Auto-generated method stub
+		/* Ett annat sätt att skriva kometarer 
+		 * 
+		 */
+		//Ett annat sätt att skriva (grönt är kommentar 
+		
+		int heltal1 =6; // Skapar en låda som heter heltal1 och fyller den med ett heltal 
+		int svar; // deklarera 
+		
+		svar= heltal1*heltal1;
+		
+		
+		System.out.println("Svar:" + heltal1*heltal1); // + tecknet kallas konkatinera 
+		
+		
+		
+		
+		
+		
+	}
+
+}
