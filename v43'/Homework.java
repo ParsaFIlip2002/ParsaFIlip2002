0a637f7a991e6586ac4baecf20d2b38e54306d4d
ParsaFIlip2002 <filip.parsa@elev.ga.ntig.se>
2019-12-09 13:00:53 +0100

    läxa

--- v43'/Homework.java
+++ v43'/Homework.java
@@ -2,19 +2,12 @@
 public class Homework {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		
-		//LOL();
+		;
 		System.out.println(max(5,2));
-	}
-	public static void LOL() {
-		System.out.println("HAHAHAHAHAH");
 	}
 
 	/**
 	 * Tar ut största talet
-	 * @param tal1
-	 * @param tal2
 	 * @return skickar största talet 
 	 */
 	public static int max(int tal1, int tal2) {
