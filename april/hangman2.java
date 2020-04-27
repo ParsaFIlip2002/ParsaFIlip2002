f0e79a2d6b5fa516f41814605784dd48f7eb827b
ParsaFIlip2002 <filip.parsa@elev.ga.ntig.se>
2020-04-27 19:16:32 +0200

    Update hangman2.java

--- april/hangman2.java
+++ april/hangman2.java
@@ -4,33 +4,36 @@ import java.util.Scanner;
 public class hangman2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-			Welcome();
-			
-			Scanner input = new Scanner(System.in);// Skannar input som kommer ifrån konsolen
-			int choice = 0;
-			try {
-				choice = input.nextInt(); // Deklarer den till int choice
-			} catch (Exception e) {
-				input.next();
-				System.out.println("Need to be an integer between 1-3");
-				System.out.println(e);
-			}
+		Welcome();
 
-			if (choice == 1) {
-					RandomWord1();
-			}
-			if (choice==2) {
-				RandomWord2();
-			}
-			if (choice==3) {
-				RandomWord3();
-			}
-	
-	
-		
-		
+		Scanner input = new Scanner(System.in);// Skannar input som kommer ifrån konsolen
+		int choice = 0;
+		try {
+			choice = input.nextInt(); // Deklarer den till int choice
+		} catch (Exception e) {
+			input.next();
+			System.out.println("Need to be an integer between 1-4");
+			System.out.println(e);
+		}
+
+		String answer;
+
+		if (choice == 1) {
+
+			answer = RandomWord1();
+		}
+		else if (choice == 2) {
+			answer = RandomWord2();
+		}
+		else if (choice == 3) {
+
+			answer = RandomWord3();
+		} else {
+			answer = "hej";
+		}
+
+		LetterChecker(answer);
 
 	}
 
@@ -46,30 +49,63 @@ public class hangman2 {
 
 	}
 
-	public static void RandomWord1() {
-	
+	public static String RandomWord1() {
+
 		String[] arr = { "saint", "error", "danger", "chant", "chart" };
 		Random r = new Random(); // Inbygd method som slumpar
 		int randomIndex = r.nextInt(arr.length); // arr.length för att slumpa 0-4
 		System.out.println(arr[randomIndex]);
 		String answer = arr[randomIndex];
+
+		return arr[randomIndex];
 	}
-	public static void RandomWord2() {
-		
+
+	public static String RandomWord2() {
+
 		String[] arr = { "betrays", "beaches", "boutons", "mondays", "advised" };
 		Random r = new Random(); // Inbygd method som slumpar
 		int randomIndex = r.nextInt(arr.length); // arr.length för att slumpa 0-4
 		System.out.println(arr[randomIndex]);
 		String answer = arr[randomIndex];
+
+		return arr[randomIndex];
 	}
-	public static void RandomWord3() {
-		
+
+	public static String RandomWord3() {
+
 		String[] arr = { "abbotship", "worldwide", "unriddled", "unguarded", "transform" };
 		Random r = new Random(); // Inbygd method som slumpar
 		int randomIndex = r.nextInt(arr.length); // arr.length för att slumpa 0-4
 		System.out.println(arr[randomIndex]);
-		String answer = arr[randomIndex];
-		
+
+		return arr[randomIndex];
+
 	}
 
+	public static void LetterChecker(String answer) {
+		for (int i = 0; i < answer.length(); i++) {
+			
+		String[] StarWord = new String[answer.length()];
+		StarWord[i] = "*";
+		System.out.print(StarWord[i]);
+		}
+		System.out.println();
+		
+		Scanner input = new Scanner(System.in);
+		String Gissning = input.nextLine();
+		
+		if (answer.contains(Gissning)) {
+			System.out.println("Right letter!");
+			}
+		 else {
+			System.out.println("Wrong letter! ");
+		
+			
+		}
+		
+		
+	}
+	
+	
+	
 }
