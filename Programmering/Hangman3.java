508a482ed8fbf23afd89b488fc586e6a1fe87b83
ParsaFIlip2002 <filip.parsa@elev.ga.ntig.se>
2020-05-08 22:09:54 +0200

    hangman

--- Programmering/Hangman3.java
+++ Programmering/Hangman3.java
@@ -1,43 +1,68 @@
 import java.util.ArrayList;
+//import java.util.Iterator;
 import java.util.Scanner;
 
-import com.sun.corba.se.spi.orbutil.fsm.Input;
+//import com.sun.corba.se.spi.orbutil.fsm.Input;
 
 public class Hangman3 {
+	// TODO restarta
+	// TODO grafiska
+	// TODO skriv ut felgissningar
 
-	private static Scanner _input = new Scanner(System.in);
-	
-	private static ArrayList<String>_wordList = new ArrayList<String>();
+	private static String progressWord = "_"; // Gör så att progressWord är globalt
+
+	private static Scanner _input = new Scanner(System.in);// globalt
+
+	private static ArrayList<String> _wordList = new ArrayList<String>();// globalt
 
 	public static void main(String[] args) {
-		printWelcomeMessage();
-		play();
-		printGoodbyeMessage();
+		printWelcomeMessage(); // Kör metod
+		play();// Kör metod
+		printGoodbyeMessage();// Kör metod
 	}
 
-	private static void play() {// om input inte lika med 0 så körs metoden startGame
+	private static void play() {
 		while (true) {
-			int difficulty = getDifficulty();
-			if (difficulty != 0) {  
+			int difficulty = getDifficulty();// hämtar en int från metoden getDifficulty och ger den variabel namnet
+												// difficulty
+			if (difficulty != 0) { // om input inte lika med 0 så körs metoden startGame
 				startGame(difficulty);
+
 			}
-			break;
+			if (difficulty == 4) {
+				play();
+
+			}
+			break; // breakar
 		}
 	}
 
-	private static void startGame(int difficulty) {
+	private static void startGame(int difficulty) { // Väljer svårighetsgrad med hjälp av int difficulty
 
 		switch (difficulty) {
 		case 1:
-			easyGame();
+			System.out.println("you have choosen level one");
+			System.out.println("to continue guess a letter");
+			easyGame();// Kör metod
+			break;
+
 		case 2:
-			normalGame();
+			System.out.println("you have choosen level two");
+			System.out.println("to continue guess a letter");
+			normalGame();// Kör metod
+			break;
+
 		case 3:
-			hardGame();
-			default:
-				System.out.println("Needs to be between 1-3");
+			System.out.println("you have choosen level three");
+			System.out.println("to continue guess a letter");
+			hardGame();// Kör metod
+			break;
+
+		default: // Skriver ut detta om inget av de ovanstående blir uppfyllda
+			System.out.println("Needs to be between 1-3");
+
+			break;
 		}
-		
 
 		// TODO Gör defualt och skriv måste va mellan 1-3
 
@@ -45,40 +70,185 @@ public class Hangman3 {
 
 	private static void easyGame() {
 
-		populateEasyWordList();
-		
-		String answer = getRandomWord();
-		System.out.println(answer);
-	
-		for (int tries = 0; tries < 10; tries++) {
-			String guess = getGuess();
-			System.out.println(guess);
-			if (guess.contains(guess)) {
-				System.out.println("GJ");
-				
+		populateEasyWordList(); // Kör metod
+
+		String answer = getRandomWord();// Kör metod och hämtar ett enkelt ord med hjälp av populateEasyWordList();
+											// och wordList som är global
+	//	System.out.println(answer);
+
+		progressWord(answer);// Kör metod
+
+		for (int tries = 0; tries < 10; tries++) {// For loop som skapar variabel inte tries. Får inte blir mer än 10
+													// och varje gång loopen körs så ökar tries
+
+			String guess = getGuess(); // initierar en variabel till metoden getGuess
+			
+			if (answer.contains(guess.toLowerCase())) { // om det man gissar finns i ordet så körs denna if sats
+				tries--; // om man har rätt så ska inte tries öka
+
+				char[] tempWord = progressWord.toCharArray(); //
+
+				for (int i = 0; i < answer.length(); i++) { // En loop som körs så många gånger som lengten på answer
+
+					if (answer.charAt(i) == guess.charAt(0)) { // kollar om charen på i är det samma som guess. Loopen
+																// kollar varje index på ordet stegvis.
+
+						tempWord[i] = guess.charAt(0); // Ersätter tempWord till guess
+
+					}
+				}
+
+				progressWord = String.copyValueOf(tempWord); //
+
 			} else {
 				System.out.println("Wrong");
+				System.out.println(tries);
 			}
 
-			// TODO läxa! ta reda på hur jag kollar om guess finns i answer, Måndag på
-			// programmering färdig. w3 school
+			System.out.println(progressWord);
 
 		}
+		printYouWon();
+
+	}
+
+	private static void progressWord(String answer) { // Skapar en String med hjälp av answer.length() så att
+														// Stringen med sträck är lika lång som answer
+		// gör till array
+		for (int i = 0; i < answer.length(); i++) {
+
+			progressWord += "_"; // concatinera plus text
+
+		}
+
+	}
+
+	private static void normalGame() {
+		populateNormalWordList(); // Kör metod
+
+		String answer = getRandomWord();// Kör metod och hämtar ett enkelt ord med hjälp av
+												// populateEasyWordList(); och wordList som är global
+		//System.out.println(answer);
+
+		progressWord(answer);// Kör metod
+
+		
+
+		for (int tries = 0; tries < 10; tries++) {// For loop som skapar variabel inte tries. Får inte blir mer än 10
+													// och varje gång loopen körs så ökar tries
+
+			String guess = getGuess(); // Kör metod och ger den namnet String guess
+
+			if (answer.contains(guess.toLowerCase())) { // om det man gissar finns i ordet så körs denna if sats
+				tries--; // om man har rätt så ska inte tries öka
+
+				char[] tempWord = progressWord.toCharArray(); //
+
+				for (int i = 0; i < answer.length(); i++) { // En loop som körs så många gånger som lengten på answer
+
+					if (answer.charAt(i) == guess.charAt(0)) { // kollar om charen på i är det samma som guess. Loopen
+																// kollar varje index på ordet stegvis.
+
+						tempWord[i] = guess.charAt(0); // Ersätter tempWord till guess
+
+						// System.out.print(newWord);
+						// System.out.print(progressWord.replace("-", guess));
+
+					}
+				}
+
+				progressWord = String.copyValueOf(tempWord); //
+
+			} else {
+				System.out.println("Wrong");
+				System.out.println(tries);
+			}
+
+			System.out.println(progressWord);
+
+		}
+
+	}
+
+	private static void hardGame() {
+		populateHardWordList(); // Kör metod
+
+		String answer = getRandomWord();// Kör metod och hämtar ett enkelt ord med hjälp av populateEasyWordList();
+											// och wordList som är global
+		//System.out.println(answer);
+
+		progressWord(answer);// Kör metod
+
+
+		for (int tries = 0; tries < 10; tries++) {// For loop som skapar variabel inte tries. Får inte blir mer än 10
+													// och varje gång loopen körs så ökar tries
+
+			String guess = getGuess(); // Kör metod och ger den namnet String guess
+
+			if (answer.contains(guess.toLowerCase())) { // om det man gissar finns i ordet så körs denna if sats
+				tries--; // om man har rätt så ska inte tries öka
+
+				char[] tempWord = progressWord.toCharArray(); //
+
+				for (int i = 0; i < answer.length(); i++) { // En loop som körs så många gånger som lengten på answer
+
+					if (answer.charAt(i) == guess.charAt(0)) { // kollar om charen på i är det samma som guess. Loopen
+																// kollar varje index på ordet stegvis.
+
+						tempWord[i] = guess.charAt(0); // Ersätter tempWord till guess
+
+						// System.out.print(newWord);
+						// System.out.print(progressWord.replace("-", guess));
+
+					}
+				}
+
+				progressWord = String.copyValueOf(tempWord); //
+
+			} else {
+				System.out.println("Wrong");
+				System.out.println(tries);
+			}
+
+			System.out.println(progressWord);
+
+		}
+
 	}
 
 	private static String getRandomWord() {
-		int range = _wordList.size();
+		int range = _wordList.size(); //
 		int randomIndex = (int) (Math.random() * range);
 		return _wordList.get(randomIndex);
 	}
 
+	
+
 	private static void populateEasyWordList() {
 		_wordList.clear();
-		_wordList.add("Hello");
-		_wordList.add("Danger");
-		_wordList.add("Chart");
+		_wordList.add("hello");
+		_wordList.add("hello");
+		_wordList.add("hello");
 		_wordList.add("error");
 		_wordList.add("saint");
+	}
+
+	private static void populateNormalWordList() {
+		_wordList.clear();
+		_wordList.add("betrays");
+		_wordList.add("beaches");
+		_wordList.add("boutons");
+		_wordList.add("mondays");
+		_wordList.add("advised");
+	}
+
+	private static void populateHardWordList() {
+		_wordList.clear();
+		_wordList.add("abbotship");
+		_wordList.add("worldwide");
+		_wordList.add("unriddled");
+		_wordList.add("unguarded");
+		_wordList.add("transform");
 	}
 
 	private static String getGuess() {
@@ -88,23 +258,15 @@ public class Hangman3 {
 		letterThatHasBeenGuessed = input.nextLine();
 
 		return letterThatHasBeenGuessed;
-		// TODO. Läxa!! get char from console
-	}
-
-	private static void normalGame() {
-
-	}
-
-	private static void hardGame() {
-
+		
 	}
 
 	private static int getDifficulty() {
-		return getValidIntegerInput(); //Skickar det den fick av getValidIntegerInput till Play
+		return getValidIntegerInput(); // Skickar det den fick av getValidIntegerInput till Play
 	}
 
-	private static int getValidIntegerInput() { //tar emot input och sedan skickar tbx till get getDifficulty
-		while (true) { 
+	private static int getValidIntegerInput() { // tar emot input och sedan skickar tbx till get getDifficulty
+		while (true) {
 			if (!_input.hasNextInt()) {
 				_input.nextLine();
 				continue;
@@ -114,7 +276,7 @@ public class Hangman3 {
 	}
 
 	private static void printWelcomeMessage() {
-		
+
 		System.out.println("Welcome to Hangman! To win you have to guess the right word");
 		System.out.println("There are three different levels:");
 		System.out.println("The level deppends on how many letters the word contains");
@@ -122,11 +284,16 @@ public class Hangman3 {
 		System.out.println("The second level has a word with seven letters");
 		System.out.println("The third level has a word with nine letters");
 		System.out.println("choose a level!");
-		
+
 	}
 
 	private static void printGoodbyeMessage() {
-		System.out.println("Good Bye!");
+		System.out.println("Hope you enjoyed this game, good bye! Press 4 to play again.");
+		play();
+	}
+
+	private static void printYouWon() {
+		System.out.println("Congratiolation you won!");
 	}
 
 }
